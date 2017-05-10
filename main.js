a30a0344b1e3ab14505486dfa9545309fdaeeaa6
Ruben <RagnarZADev@gmail.com>
2017-05-10 14:07:32 +0200

    1.0.0

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
-// Retrieve the template data from the HTML (jQuery is used here).
+module.exports = function(width, height) {
+    return width * height;
+};
+
 var template = $('#handlebars-demo').html();
 
 
 // Compile the template data into a function
 var templateScript = Handlebars.compile(template);
 
-
-var context = { "name" : "Ritesh Kumar", "occupation" : "developer" };
+var context =  {
+    "person" : [
+        {
+            "name" : "Ruben",
+            "city" : "Pretoria",
+            "food" : "Biltong",
+            "image" : "<img src='index.jpeg'>"
+        },
+        {
+            "name" : "Schan",
+            "city" : "Cape Town",
+            "food" : "Pizza",
+            "image" : "<img src='index.jpeg'>"
+        },
+        {
+            "name" : "Reinhardt",
+            "city" : "Pretoria",
+            "food" : "Noodles",
+            "image" : "<img src='index.jpeg'>"
+        },
+        {
+            "name" : "Don",
+            "city" : "Somerset Wes",
+            "food" : "Sushi",
+            "image" : "<img src='index.jpeg'>"
+        },
+        {
+            "name" : "Jason",
+            "city" : "Table View",
+            "food" : "Lamb Chops",
+            "image" : "<img src='index.jpeg'>"
+        },
+        {
+            "name" : "Shipo",
+            "city" : "Soweto",
+            "food" : "Pap",
+            "image" : "<img src='index.jpeg'>"
+        }
+    ]
+};
 
 // html = 'My name is Ritesh Kumar. I am a developer.'
 var html = templateScript(context);
 
 
 // Insert the HTML code into the page
-$('#test').append(html);
+$('#test').html(html);
