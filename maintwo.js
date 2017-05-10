c6212e6b473b96ead6f1e1d17e6193d53c0b932c
Ruben <RagnarZADev@gmail.com>
2017-05-10 14:16:34 +0200

    1.0.0

--- maintwo.js
+++
@@ -1,9 +0,0 @@
-var templatetwo = $('#handlebars-two').html();
-
-var templateScriptTwo = Handlebars.compile(templatetwo);
-
-var contexttwo = { "name" : "Ruben", "occupation" : "developer" };
-
-var htmltwo = templateScript(contexttwo);
-
-$('#testtwo').append(htmltwo);
